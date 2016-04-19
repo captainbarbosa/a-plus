12340e7ac7ae3be5a1d00b80872202518ee05350
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-19 18:32:46 -0400

    Fixing an oopsie on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :students
   resources :grades
   resources :parents
-  resources :parents
-  
+  resources :teachers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
