df5dffb5a0d9456c605ec2673fb5b04b6d1ccf33
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-19 16:48:41 -0400

    teachers have many students

--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TeacherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "teachers have many students" do
+    teacher = teachers(:mr_math_man)
+    student_1 = students(:jimmy_mcjim)
+    student_2 = students(:carl_cannoli)
+
+    assert_equal teacher.id, student_1.teacher_id
+    assert_equal teacher.id, student_2.teacher_id
+  end
+
+  test "teachers have many grades" do
+
+  end
 end
