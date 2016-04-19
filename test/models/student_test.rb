405f221479583856c08a4a8bae89463551791868
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-19 16:22:16 -0400

    student belongs to a teacher

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "student belongs to teacher" do
+    student = students(:alice_smith)
+    teacher = teachers(:mrs_science_lady)
+
+    assert_equal student.teacher_id, teacher.id
+  end
+
+  test "student has many parents" do
+
+  end
+
+  test "student has many grades" do
+
+  end
 end
