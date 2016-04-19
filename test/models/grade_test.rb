453423031635a50a7d6320fea658638796b42137
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-19 15:39:35 -0400

    Grade belongs to a student

--- test/models/grade_test.rb
+++ test/models/grade_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class GradeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "grade belongs to a student" do
+    grade = grades(:grade_1)
+    student = students(:Alice_Smith)
+
+    assert_equal grade.student_id, student.id
+  end
 end
