b428751273d6869430a410435366b99c9b8eb2e2
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-19 20:32:40 -0400

    Rename column in users table, add Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419195208) do
+ActiveRecord::Schema.define(version: 20160420002715) do
 
   create_table "grades", force: :cascade do |t|
     t.string   "assignment_name"
@@ -42,5 +42,17 @@ ActiveRecord::Schema.define(version: 20160419195208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "login"
+    t.string   "password_digest"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
