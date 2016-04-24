f2bcf8f2330e3fc11e91bbae6dad85583eb92044
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-24 11:52:51 -0400

    Add form and navbar styles

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#dashboard'
+  root 'grades#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
